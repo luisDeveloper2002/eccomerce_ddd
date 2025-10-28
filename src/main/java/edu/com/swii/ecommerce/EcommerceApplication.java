469cb4362a4fc8b6d7a48bc2b3e8574aa1bc3496
LUIS <luis.hernandez09@uptc.edu.co>
2025-10-28 15:01:32 -0500

    first commit

---
+++ src/main/java/edu/com/swii/ecommerce/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package edu.com.swii.ddd_full;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/com/swii/ddd_full/DddFullApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.com.swii.ddd_full;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
